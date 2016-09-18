e658d95416b671c3d77dba75f81bd7beb9b72066
Julien Roland <julien.roland@isen-lille.fr>
2016-09-18 17:44:12 +0200

    Add vundle and font setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,14 @@ files=".tmux.conf .zshrc .vimrc .gitconfig .emacs"
 directories=".atom"
 scriptname=$(readlink -f $0)
 dotfilesdir=$(dirname $scriptname)
+vundle_dir=~/.vim/bundle/Vundle.vim
+
+function setup_fonts {
+  git clone https://github.com/powerline/fonts /tmp/fonts
+  mkdir -p ~/.fonts
+  mv /tmp/fonts/SourceCodePro/*.otf ~/.fonts
+  fc-cache -vf ~/.fonts/
+}
 
 for file in $files; do
   ln -si $dotfilesdir/$file ~/$file
@@ -14,3 +22,9 @@ for directory in $directories; do
     ln -si $dotfilesdir/$directory/$file ~/$directory/$file
   done
 done
+
+if test ! -e $vundle_dir; then
+  git clone https://github.com/VundleVim/Vundle.vim.git $vundle_dir
+fi
+
+setup_fonts
